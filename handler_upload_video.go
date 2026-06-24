1e4cea7267cc57d1b3d6f79a855e1b35eaaa342a
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-06-23 21:37:25 -0400

    Added function to process a video for fast start before uploading

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -160,3 +160,21 @@ func getVideoAspectRatio(filePath string) (string, error) {
 	// is other
 	return "other", nil
 }
+
+func processVideoForFastStart(filePath string) (string, error) {
+	outputPath := filePath + ".processing"
+
+	// The command is ffmpeg and the arguments are:
+	// -i, the input file path, 
+	// -c, copy, 
+	// -movflags, faststart, 
+	// -f, mp4 
+	// and the output file path.
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy","-movflags", "faststart", "-f", "mp4", outputPath)
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return outputPath, nil
+}
